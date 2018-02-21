08814797594c9e5bfce0cee4da656ebfa1975171
e_lobanova <e_lobanova@skbkontur.ru>
2018-02-21 16:43:43 +0500

    add file

---
+++ EchelonAcknowledgeResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Kontur.Echelon
+{
+    public enum EchelonAcknowledgeResult
+    {
+        Success = 0,
+        ServiceUnavailable = 1,
+        Unauthenticated = 2,
+        UnknownError = 3
+    }
+}
