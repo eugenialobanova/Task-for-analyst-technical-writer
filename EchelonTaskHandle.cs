23475b62efe9bc7dc0965e2252472eb4a58a9922
e_lobanova <e_lobanova@skbkontur.ru>
2018-02-21 16:56:47 +0500

    new files

---
+++ EchelonTaskHandle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Kontur.Logging;
+using Kontur.Net.Http;
+using Kontur.Utilities.Convertions.Time;
+
+namespace Kontur.Echelon
+{
+    internal class EchelonTaskHandle : IEchelonTaskHandle
+    {
+        private readonly IClusterClientsForShardsResolver clientResolver;
+        private readonly EchelonClientRequestFactory requestFactory;
+        private readonly ILog log;
+
+        public EchelonTaskHandle(EchelonTask task, IClusterClient shardClient, EchelonClientRequestFactory requestFactory, ILog log, EchelonTaskMeta meta = null, EchelonTaskOptions options = null)
+        {
+            Task = task;
+            Meta = meta;
+            Options = options;
+            this.requestFactory = requestFactory;
+            this.log = log;
+
+            clientResolver = new OneClusterClientResolver(shardClient);
+        }
+
+        public EchelonTask Task { get; }
+        public EchelonTaskOptions Options { get; }
+        public EchelonTaskMeta Meta { get; }
+
+        public Task<EchelonAcknowledgeResult> AcknowledgeAsync(TimeSpan timeout)
+        {
+            return new AcknowledgeOperation(clientResolver, requestFactory, log).ExecuteAsync(new[] {Task.Id}, timeout);
+        }
+
+        public Task<EchelonProlongResult> ProlongExecutionAsync(TimeSpan duration)
+        {
+            return ProlongExecutionAsync(duration as TimeSpan?);
+        }
+
+        public Task<EchelonProlongResult> ProlongExecutionAsync()
+        {
+            return ProlongExecutionAsync(null);
+        }
+
+        public Task<EchelonPostponeResult> PostponeAsync(TimeSpan duration)
+        {
+            return PostponeAsync(duration as TimeSpan?);
+        }
+
+        public Task<EchelonPostponeResult> PostponeAsync()
+        {
+            return PostponeAsync(null);
+        }
+
+        private Task<EchelonProlongResult> ProlongExecutionAsync(TimeSpan? duration)
+        {
+            var request = new ProlongExecutionRequest(Task.Id, duration);
+            var operation = new ProlongOperation(clientResolver, requestFactory, log);
+            return operation.ExecuteAsync(request, 30.Seconds());
+        }
+
+        private Task<EchelonPostponeResult> PostponeAsync(TimeSpan? duration)
+        {
+            var request = new PostponeRequest(Task.Id, duration);
+            var operation = new PostponeOperation(clientResolver, requestFactory, log);
+            return operation.ExecuteAsync(request, 30.Seconds());
+        }
+    }
+}
---
+++ IEchelonTaskHandle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kontur.Echelon
+{
+    public interface IEchelonTaskHandle
+    {
+        EchelonTask Task { get; }
+
+        EchelonTaskOptions Options { get; }
+
+        EchelonTaskMeta Meta { get; }
+
+        Task<EchelonAcknowledgeResult> AcknowledgeAsync(TimeSpan timeout);
+
+        Task<EchelonProlongResult> ProlongExecutionAsync(TimeSpan duration);
+
+        Task<EchelonProlongResult> ProlongExecutionAsync();
+
+        Task<EchelonPostponeResult> PostponeAsync(TimeSpan duration);
+
+        Task<EchelonPostponeResult> PostponeAsync();
+    }
+}
